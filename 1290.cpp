6faa8d7eb9effcbd0569aa41072a9a7f205074ba
Nandan Kabra <kabranandan2006@gmail.com>
2026-03-24 11:38:59 +0530

    Create 1290.cpp

---
+++ 1290.cpp
@@ -0,0 +1,17 @@
+// Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1.
+// The linked list holds the binary representation of a number.
+// Return the decimal value of the number in the linked list.
+// The most significant bit is at the head of the linked list.
+
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int result=0;
+        ListNode* temp=head;
+        while(temp!=nullptr){
+            result=result*2+temp->val;
+            temp=temp->next;
+        }
+        return result;
+    }
+};
