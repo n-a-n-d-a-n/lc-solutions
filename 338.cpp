f83d3469259f8cead0d899221b9fe12e6f270713
Nandan Kabra <kabranandan2006@gmail.com>
2026-04-27 02:08:38 +0530

    Create 338.cpp

---
+++ 338.cpp
@@ -0,0 +1,14 @@
+// Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n),
+// ans[i] is the number of 1's in the binary representation of i.
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n+1);
+        ans[0]=0;
+        for(int i=1;i<=n;i++){
+            ans[i]=ans[i>>1]+(i&1);
+        }
+        return ans;
+    }
+};
