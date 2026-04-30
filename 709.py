772711d52605178c29fa335feb8fbd085eb4be21
Nandan Kabra <kabranandan2006@gmail.com>
2026-04-30 10:14:01 +0530

    Create 709.py

---
+++ 709.py
@@ -0,0 +1,5 @@
+# Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+class Solution:
+  def toLowerClass(self,s:str)->str:
+    return s.lower()
