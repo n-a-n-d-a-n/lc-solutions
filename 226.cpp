4e107a590b0d00df941cc51305466f683267de28
Nandan Kabra <kabranandan2006@gmail.com>
2026-06-02 19:52:22 +0530

    Create 226.cpp

---
+++ 226.cpp
@@ -0,0 +1,16 @@
+// Given the root of a binary tree, invert the tree, and return its root.
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==nullptr){
+            return NULL;
+        }
+        TreeNode* temp=root->left;
+        root->left=root->right;
+        root->right=temp;
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
+};
