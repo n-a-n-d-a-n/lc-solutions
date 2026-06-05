2a8bbe5c86672c2010559e0fb75dabcdd28031ac
Nandan Kabra <kabranandan2006@gmail.com>
2026-06-06 01:15:47 +0530

    Create 287.cpp

---
+++ 287.cpp
@@ -0,0 +1,17 @@
+// Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+// There is only one repeated number in nums, return this repeated number.
+// You must solve the problem without modifying the array nums and using only constant extra space.
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n-1;i++){
+            if(nums[i]==nums[i+1]){
+                return nums[i];
+            }
+        }
+        return 0;
+    }
+};
