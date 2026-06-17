9f5c533a78d1323111ab23f2069a8ef586f4dac5
Nandan Kabra <kabranandan2006@gmail.com>
2026-06-17 17:03:21 +0530

    Create 2886.py

---
+++ 2886.py
@@ -0,0 +1,18 @@
+# DataFrame students
+# +-------------+--------+
+# | Column Name | Type   |
+# +-------------+--------+
+# | student_id  | int    |
+# | name        | object |
+# | age         | int    |
+# | grade       | float  |
+# +-------------+--------+
+# Write a solution to correct the errors:
+# The grade column is stored as floats, convert it to integers.
+# The result format is in the following example.
+
+import pandas as pd
+
+def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
+    students['grade'] = students['grade'].astype(int)
+    return students
