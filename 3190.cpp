b6737105d77c995acb97639571254e3f5690a4a0
Nandan Kabra <kabranandan2006@gmail.com>
2026-06-18 16:46:57 +0530

    Update 3190.cpp

--- 3190.cpp
+++ 3190.cpp
@@ -25,3 +25,16 @@ public:
         return count;
     }
 };
+
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        int count=0;
+        for(int num:nums){
+            if(num%3!=0){
+                count++;
+            }
+        }
+        return count;
+    }
+};
