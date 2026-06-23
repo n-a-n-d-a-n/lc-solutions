5a40b0aad9468726bcab979366166a4a2d06aacb
Nandan Kabra <kabranandan2006@gmail.com>
2026-06-23 12:15:11 +0530

    Create 1641.cpp

---
+++ 1641.cpp
@@ -0,0 +1,10 @@
+// Given an integer n, return the number of strings of length n that consist only of vowels (a, e, i, o, u) and are lexicographically sorted.
+// A string s is lexicographically sorted if for all valid i, s[i] is the same as or comes before s[i+1] in the alphabet.
+
+class Solution {
+public:
+    int countVowelStrings(int n) {
+        int ans=(n+4)*(n+3)*(n+2)*(n+1)/24;
+        return ans;
+    }
+};
