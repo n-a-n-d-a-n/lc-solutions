3d88c203688ba562e4aa81a782fa3a7b57219516
Nandan Kabra <kabranandan2006@gmail.com>
2026-06-29 15:00:40 +0530

    Add solution for #1967 - Number of Strings That Are Substrings of a Given String

---
+++ array/1967-number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -0,0 +1,19 @@
+﻿// LeetCode 1967 - Number of Strings That Are Substrings of a Given String
+// https://leetcode.com/problems/number-of-strings-that-are-substrings-in-word/
+// Description: Given an array of strings `patterns` and a string `word`,
+// return the number of strings in patterns that exist as a substring in word.
+// Approach: brute force - check each pattern with string::find
+// Time: O(n*m) | Space: O(1)
+
+class Solution {
+public:
+    int numOfStrings(vector<string>& patterns, string word) {
+        int count=0;
+        for(string pat:patterns){
+            if(word.find(pat)!=string::npos){
+                count++;
+            }
+        }
+        return count;
+    }
+};
